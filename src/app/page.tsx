462dc4d402b5162a8678752141a03a80399b0d09
VeerSingh2104 <singh.lakshya2104@gmail.com>
2026-09-01 22:04:04 +0530

    Updated app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,16 @@
+import { redirect } from 'next/navigation'
 import { createClient } from '@/lib/supabase/server'
 
-export default async function Home() {
+export default async function HomePage() {
   const supabase = await createClient()
 
   const {
-  data: { user },
-} = await supabase.auth.getUser()
+    data: { user },
+  } = await supabase.auth.getUser()
 
-  return (
-  <main className="p-10">
-    <h1 className="text-2xl font-bold">
-      Supabase Connection Test
-    </h1>
+  if (user) {
+    redirect('/dashboard')
+  }
 
-    <p className="mt-4">
-      {user
-        ? `Logged in as: ${user.email}`
-        : 'Supabase connected — no user is logged in.'}
-    </p>
-  </main>
-)
+  redirect('/login')
 }
